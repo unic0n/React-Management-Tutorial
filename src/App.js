b78b9fbffcda50b5c5f306d1e709cd089a07cdb9
unic0n <changjs@naver.com>
2020-06-25 21:40:36 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="puple-background">
+      <img src={logo} />
+      <h2>Let's develop management system!!!!!!</h2>
+    </div>
+  );
+}
+
+export default App;
